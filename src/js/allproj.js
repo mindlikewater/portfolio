d1eaa084bbdd164c7b4546ec5c9c461f102a4eec
Tai Chen <mindlikewater@gmail.com>
2016-12-28 20:47:02 -0500

    Contains array of top projects on Github

---
+++ src/js/allproj.js
@@ -0,0 +1,34 @@
+const PROJECTS = [
+	//THE eXCHANGE
+	{
+		link: 'THE eXCHANGE',
+		title: 'THE eXCHANGE',
+		description: "THE eXCHANGE is an organizational app that can be used for forming or finding Bible Study groups with the purpose of community-building in small groups.",
+		website: 'https://tiy-gracelee-the-exchange-frontend.surge.sh/#/home',
+    github code: 'https://github.com/mindlikewater/the-exchange-frontend',
+		image: './images/theexchange.png',
+		tools: "AngularJS, jQuery, Bootstrap, Moment, AdonisJS, NodeJS, Google Maps API"
+	},
+	//Snapogram
+	{
+		link: 'Snapogram',
+		title: 'Snapogram',
+		description: "Snapogram is an Instagram clone that was created during a hackathon weekend. Users can share their favorite images in the shape of a polaroid picture and add likes by clicking the heart. Hovering over the image shows the image's title, and clicking takes the user to an individual image page.",
+		website: 'https://tiy-daqundamagwood-snapogram.surge.sh/#/',
+		github code: 'https://github.com/mindlikewater/snapogram',
+		image: './images/snapogramimg.png',
+		tools: "AngularJS, jQuery, AdonisJS, NodeJS, Bulma"
+	},
+//Brews on Board restaurant
+	{
+		link: 'Brews on Board',
+		title: 'Brews on Board restaurant',
+		description: "Brews on Board is a restaurant website made from scratch based on a wireframe.",
+		website: 'https://tiy-mikaelchen-wk5-proj-restaurant.surge.sh/',
+		github code: 'https://github.com/mindlikewater/TIY-wk5-restaurant',
+		image: './images/bob-restaurant.png',
+		tools: "jQuery, Google Maps API, Heroku API, flickr API"
+	}
+]
+
+export { PROJECTS }
